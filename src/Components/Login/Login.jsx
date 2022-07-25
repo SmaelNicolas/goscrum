94f54b7857f76e974809288b73b5a71613444a7d
Smael Nicolas <nico.smael@gmail.com>
2022-07-25 16:21:57 -0300

    add formik validations

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { useFormik } from "formik";
 
 export const Login = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	// const [email, setEmail] = useState("");
+	// const [password, setPassword] = useState("");
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (!email || !password) {
-			alert("INCOMPLETO");
-		} else {
-			window.location = "https://google.com.ar";
-		}
+	const initialValues = {
+		email: "",
+		password: "",
 	};
+
+	const validate = (values) => {
+		const errors = {};
+
+		if (!values.email) {
+			errors.email = "El email es requerido";
+		}
+		if (!values.password) {
+			errors.password = "El password es requerido";
+		}
+
+		return errors;
+	};
+
+	const onSubmit = () => {
+		localStorage.setItem("logged", "yes");
+	};
+
+	const formik = useFormik({ initialValues, validate, onSubmit });
+
+	const { handleSubmit, handleChange, values, errors } = formik;
 
 	return (
 		<form onSubmit={handleSubmit}>
@@ -21,18 +38,20 @@ export const Login = () => {
 				<input
 					name='email'
 					type='email'
-					value={email}
-					onChange={(e) => setEmail(e.currentTarget.value)}
+					value={values.email}
+					onChange={handleChange}
 				/>
+				{errors.email && <div>{errors.email}</div>}
 			</div>
 			<div>
 				<label>Contraseña</label>
 				<input
 					type='password'
 					name='password'
-					value={password}
-					onChange={(e) => setPassword(e.currentTarget.value)}
+					value={values.password}
+					onChange={handleChange}
 				/>
+				{errors.password && <div>{errors.password}</div>}
 			</div>
 			<div>
 				<button type='submit'>Enviar</button>
