a56d641f2b2bdd78563abc501ad45e09bfe6646f
Smael Nicolas <nico.smael@gmail.com>
2022-07-25 19:47:47 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
-import { Login } from "./Components/Login/Login";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./Components/views/Login/Login";
+import { Register } from "./Components/Register/Register";
+import { Error404 } from "./Components/views/Error404/Error404";
+import { Tasks } from "./Components/views/Tasks/Tasks";
+
+const RequireAuth = ({ children }) => {
+	if (!localStorage.getItem("logged")) {
+		return <Navigate to='/login' replace={true} />;
+	}
+	return children;
+};
 
 export const App = () => {
 	return (
 		<div className='App'>
-			<Login />
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<RequireAuth>
+							<Tasks />
+						</RequireAuth>
+					}
+				/>
+				<Route path='/login' element={<Login />} />
+				<Route path='*' element={<Error404 />} />
+				<Route path='/register' element={<Register />} />
+			</Routes>
 		</div>
 	);
 };
