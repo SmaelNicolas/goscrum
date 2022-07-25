61990b3d1f88ae1e2f9dbd56562913c93705a635
Smael Nicolas <nico.smael@gmail.com>
2022-07-25 19:53:46 -0300

    add animation to every route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Register } from "./Components/Register/Register";
 import { Error404 } from "./Components/views/Error404/Error404";
 import { Tasks } from "./Components/views/Tasks/Tasks";
 
+import { AnimatePresence, motion } from "framer-motion";
+
 const RequireAuth = ({ children }) => {
 	if (!localStorage.getItem("logged")) {
 		return <Navigate to='/login' replace={true} />;
@@ -12,22 +14,80 @@ const RequireAuth = ({ children }) => {
 	return children;
 };
 
+const pageTransition = {
+	in: {
+		opacity: 1,
+	},
+	out: {
+		opacity: 0,
+	},
+};
+
 export const App = () => {
 	return (
 		<div className='App'>
-			<Routes>
-				<Route
-					path='/'
-					element={
-						<RequireAuth>
-							<Tasks />
-						</RequireAuth>
-					}
-				/>
-				<Route path='/login' element={<Login />} />
-				<Route path='*' element={<Error404 />} />
-				<Route path='/register' element={<Register />} />
-			</Routes>
+			<AnimatePresence>
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<RequireAuth>
+								<motion.div
+									className='page'
+									initial='out'
+									animate='in'
+									exit='out'
+									variant={pageTransition}
+								>
+									<Tasks />
+								</motion.div>
+							</RequireAuth>
+						}
+					/>
+					<Route
+						path='/login'
+						element={
+							<motion.div
+								className='page'
+								initial='out'
+								animate='in'
+								exit='out'
+								variant={pageTransition}
+							>
+								<Login />
+							</motion.div>
+						}
+					/>
+					<Route
+						path='*'
+						element={
+							<motion.div
+								className='page'
+								initial='out'
+								animate='in'
+								exit='out'
+								variant={pageTransition}
+							>
+								<Error404 />
+							</motion.div>
+						}
+					/>
+					<Route
+						path='/register'
+						element={
+							<motion.div
+								className='page'
+								initial='out'
+								animate='in'
+								exit='out'
+								variant={pageTransition}
+							>
+								<Register />
+							</motion.div>
+						}
+					/>
+				</Routes>
+			</AnimatePresence>
 		</div>
 	);
 };
