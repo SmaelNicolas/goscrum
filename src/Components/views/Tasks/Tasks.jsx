3c28aab673461ffbb463d3e842a1c896fd65ab6e
Smael Nicolas <nico.smael@gmail.com>
2022-07-26 14:47:46 -0300

    replace cards with component

--- src/Components/views/Tasks/Tasks.jsx
+++ src/Components/views/Tasks/Tasks.jsx
@@ -20,7 +20,7 @@ export const Tasks = () => {
 					<section>
 						<div id='list cards'>
 							<h4>EN TELEFONO</h4>
-							<Card />
+							<Card fnReduce={reduceString} />
 						</div>
 					</section>
 				) : (
@@ -28,57 +28,15 @@ export const Tasks = () => {
 						<h2>Mis Tareas</h2>
 						<div id='list cards'>
 							<h4>Nuevas</h4>
-							<div id='card'>
-								<div>X</div>
-								<h3>Tarea 1</h3>
-								<h6>24/1/2022 16:40</h6>
-								<h5>Nicolas Smael</h5>
-								<button type='button'> Nueva</button>
-								<button type='button'> Alta/media/baja</button>
-								<p>
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Nobis officia, ipsa
-									laboriosam explicabo exercitationem aliquam
-									rerum blanditiis amet animi necessitatibus
-									minima. Doloribus labore iste placeat!
-								</p>
-							</div>
+							<Card fnReduce={reduceString} />
 						</div>
 						<div id='list cards'>
 							<h4>En Proceso</h4>
-							<div id='card'>
-								<div>X</div>
-								<h3>Tarea 1</h3>
-								<h6>24/1/2022 16:40</h6>
-								<h5>Nicolas Smael</h5>
-								<button type='button'> Nueva</button>
-								<button type='button'> Alta/media/baja</button>
-								<p>
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Nobis officia, ipsa
-									laboriosam explicabo exercitationem aliquam
-									rerum blanditiis amet animi necessitatibus
-									minima. Doloribus labore iste placeat!
-								</p>
-							</div>
+							<Card fnReduce={reduceString} />
 						</div>
 						<div id='list cards'>
 							<h4>Terminadas</h4>
-							<div id='card'>
-								<div>X</div>
-								<h3>Tarea 1</h3>
-								<h6>24/1/2022 16:40</h6>
-								<h5>Nicolas Smael</h5>
-								<button type='button'> Nueva</button>
-								<button type='button'> Alta/media/baja</button>
-								<p>
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Nobis officia, ipsa
-									laboriosam explicabo exercitationem aliquam
-									rerum blanditiis amet animi necessitatibus
-									minima. Doloribus labore iste placeat!
-								</p>
-							</div>
+							<Card fnReduce={reduceString} />
 						</div>
 					</section>
 				)}
