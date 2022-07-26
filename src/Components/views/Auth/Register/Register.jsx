7f0fd0d5311c546fe0fb13f7f7dbc123015ae686
Smael Nicolas <nico.smael@gmail.com>
2022-07-26 20:41:25 -0300

    fetch api to register user

--- src/Components/views/Auth/Register/Register.jsx
+++ src/Components/views/Auth/Register/Register.jsx
@@ -1,11 +1,14 @@
 // https://formik.org/docs/overview
 import { useFormik } from "formik";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
+import { v4 as uuidv4 } from "uuid";
+import { Switch, FormControlLabel } from "@mui/material";
 
 export const Register = () => {
 	const [data, setData] = useState();
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		fetch("https://goscrum-api.alkemy.org/auth/data")
@@ -13,16 +16,15 @@ export const Register = () => {
 			.then((dat) => setData(dat.result));
 	}, []);
 
-	console.log(data);
-
 	const initialValues = {
 		userName: "",
 		password: "",
 		email: "",
-		// teamID: "",
+		teamID: "",
 		role: "",
 		continent: "",
 		region: "",
+		switch: false,
 	};
 
 	const requiered = "* Campo Obligatorio";
@@ -36,19 +38,53 @@ export const Register = () => {
 			email: Yup.string()
 				.email("Debe ser un email valido")
 				.required(requiered),
+			// teamID: Yup.string().required(requiered),
 			role: Yup.string().required(requiered),
 			continent: Yup.string().required(requiered),
 			region: Yup.string().required(requiered),
 		});
 
+	const handleChangeContinent = (value) => {
+		setFieldValue("continent", value);
+		if (value !== "America") setFieldValue("region", "Otro");
+	};
+
 	const onSubmit = () => {
+		const teamID = values.teamID ? values.teamID : uuidv4();
+		fetch("https://goscrum-api.alkemy.org/auth/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				user: {
+					userName: values.userName,
+					password: values.password,
+					email: values.email,
+					teamID,
+					role: values.role,
+					continent: values.continent,
+					region: values.region,
+				},
+			}),
+		})
+			.then((response) => response.json())
+			.then(navigate("/registered", { replace: true }));
+
 		alert();
 	};
 
 	const formik = useFormik({ initialValues, validationSchema, onSubmit });
 
-	const { handleSubmit, handleChange, values, errors, touched, handleBlur } =
-		formik;
+	const {
+		handleSubmit,
+		handleChange,
+		values,
+		errors,
+		touched,
+		handleBlur,
+		setFieldValue,
+	} = formik;
 
 	return (
 		<form onSubmit={handleSubmit}>
@@ -112,7 +148,9 @@ export const Register = () => {
 				<select
 					name='continent'
 					value={values.continent}
-					onChange={handleChange}
+					onChange={(e) =>
+						handleChangeContinent(e.currentTarget.value)
+					}
 					onBlur={handleBlur}
 				>
 					<option value=''>Seleccionar continente ...</option>
@@ -147,7 +185,35 @@ export const Register = () => {
 					)}
 				</div>
 			)}
-			<input type='hidden' name='teamId' value='' />
+			{
+				<FormControlLabel
+					control={
+						<Switch
+							value={values.switch}
+							onChange={() =>
+								formik.setFieldValue(
+									"switch",
+									!formik.values.switch
+								)
+							}
+							name='switch'
+							color='secondary'
+						/>
+					}
+					label='Perteneces a un equipo ya creado'
+				/>
+			}
+			{values.switch && (
+				<div>
+					<label htmlFor=''>Identificador del equipo</label>
+					<input
+						type='text'
+						name='teamId'
+						value={values.teamID}
+						onChange={handleChange}
+					/>
+				</div>
+			)}
 
 			<div>
 				<button type='submit'>Enviar</button>
