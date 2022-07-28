bdbb340837074325cdf956e7bf2c09d09067c0b0
Smael Nicolas <nico.smael@gmail.com>
2022-07-28 19:48:53 -0300

    add name user logged in

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
 	const navigate = useNavigate();
+	const [user, setUser] = useState(localStorage.getItem("user"));
 	const handleLogOut = () => {
 		localStorage.removeItem("logged");
+		localStorage.removeItem("user");
 		navigate("/login", { replace: true });
 	};
 
 	return (
 		<header>
 			<span>Go Scrum</span>
-			<div onClick={handleLogOut}>X</div>
+			<div>
+				<div>{user}</div>
+				<div onClick={handleLogOut}>X</div>
+			</div>
 		</header>
 	);
 };
