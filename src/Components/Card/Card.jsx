00020d89ea9c2039f2c86bfaced1f0d5bf979782
Smael Nicolas <nico.smael@gmail.com>
2022-07-28 20:05:28 -0300

    add control long texts

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,4 @@
-const reduceString = (str) => {
-	return str.length > 300
-		? { string: str.slice(0, 297).concat("..."), addButton: true }
-		: { string: str, addButton: false };
-};
+import { useState } from "react";
 
 export const Card = ({
 	data: {
@@ -15,15 +11,29 @@ export const Card = ({
 		importance,
 	},
 }) => {
+	const dateTime = new Date(createdAt).toLocaleString() + "hs.";
+	const [showMore, setShowMore] = useState(false);
+
+	const reduceString = (str) => {
+		return str.length > 150
+			? { string: str.slice(0, 147).concat("..."), addButton: true }
+			: { string: str, addButton: false };
+	};
+
 	return (
 		<div id='card'>
 			<div>X</div>
 			<h3>{title}</h3>
-			<h6>{createdAt}</h6>
+			<h6>{dateTime}</h6>
 			<h5>{userName}</h5>
 			<button type='button'> {status}</button>
 			<button type='button'> {importance}</button>
-			<p>{reduceString(description).string}</p>
+			<p style={{ width: "500px" }}>
+				{!showMore ? reduceString(description).string : description}
+			</p>
+			<button onClick={() => setShowMore(!showMore)}>
+				{!showMore ? "Ver Mas" : "Ver Menos"}
+			</button>
 		</div>
 	);
 };
