483ccef6adab246179b87a0f3eded81f8de25e58
Smael Nicolas <nico.smael@gmail.com>
2022-07-28 19:48:42 -0300

    add debounce, search filter

--- src/Components/views/Tasks/Tasks.jsx
+++ src/Components/views/Tasks/Tasks.jsx
@@ -5,16 +5,26 @@ import { Header } from "../../Header/Header";
 import { TaskForm } from "../../TaskForm/TaskForm";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import debounce from "lodash.debounce";
+import {
+	FormControl,
+	FormControlLabel,
+	Radio,
+	RadioGroup,
+} from "@mui/material";
 
 const { REACT_APP_API_ENDPOINT: API_ENDPOINT } = process.env;
 
 export const Tasks = () => {
-	const [list, setList] = useState([]);
 	const { isMobile } = useResize();
+	const [list, setList] = useState([]);
+	const [listByType, setListByType] = useState([]);
+	const [taskMadeBy, setTaskMAdeBy] = useState("ALL");
 	const [loading, setLoading] = useState(true);
+	const [search, setSearch] = useState();
 
 	useEffect(() => {
-		fetch(`${API_ENDPOINT}task`, {
+		fetch(`${API_ENDPOINT}task${taskMadeBy === "ME" ? "/me" : ""}`, {
 			headers: {
 				"Content-Type": "application/json",
 				Authorization: "Bearer " + localStorage.getItem("token"),
@@ -23,27 +33,93 @@ export const Tasks = () => {
 			.then((response) => response.json())
 			.then((data) => {
 				setList(data.result);
+				setListByType(data.result);
 				setTimeout(() => {
 					setLoading(false);
 				}, 5000);
 			});
-	}, []);
+	}, [taskMadeBy]);
+
+	useEffect(() => {
+		if (search) {
+			setListByType(
+				list.filter((task) => task.title.toLowerCase().includes(search))
+			);
+		} else {
+			setListByType(list);
+		}
+	}, [search]);
 
 	const renderAllCards = () => {
 		return list?.map((data) => <Card key={data._id} data={data} />);
 	};
 	const renderCardsByType = (value) => {
-		return list
+		return listByType
 			?.filter((card) => card.status === value)
 			.map((data) => <Card key={data._id} data={data} />);
 	};
+
+	const handleChangeImportance = (importance) => {
+		importance !== "ALL"
+			? setListByType(
+					list.filter((task) => task.importance === importance)
+			  )
+			: setListByType(list);
+	};
+
+	const handleSearch = debounce((value) => {
+		setSearch(value);
+	}, 2000);
 
 	return (
 		<div>
 			<Header />
 			<TaskForm />
 			<main id='tasks'>
-				{/* wrapperlist */}
+				<h2>Mis tareas</h2>
+				<div>
+					<FormControl>
+						<RadioGroup
+							row
+							aria-labelledby='demo-row-radio-buttons-group-label'
+							onChange={(e) => {
+								setTaskMAdeBy(e.currentTarget.value);
+							}}
+						>
+							<FormControlLabel
+								value='ALL'
+								control={<Radio />}
+								label='Todas'
+							/>
+							<FormControlLabel
+								value='ME'
+								control={<Radio />}
+								label='Mis tareas'
+							/>
+						</RadioGroup>
+					</FormControl>
+					<div>
+						<input
+							type='text'
+							placeholder='Buscar por titulo ...'
+							onChange={(e) => {
+								handleSearch(e?.target?.value);
+							}}
+						/>
+					</div>
+					<select
+						name='importance'
+						onChange={(e) => {
+							handleChangeImportance(e.currentTarget.value);
+						}}
+					>
+						<option value=''>Seleccionar una prioridad</option>
+						<option value='ALL'>Todas</option>
+						<option value='LOW'>Baja</option>
+						<option value='MEDIUM'>Media</option>
+						<option value='HIGH'>Alta</option>
+					</select>
+				</div>
 				{!list.length ? (
 					<div>No existen tareas creadas</div>
 				) : loading ? (
