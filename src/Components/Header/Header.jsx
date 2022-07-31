29b8081bf14a7bbb1a15fce4876a4f563a8f57c9
Smael Nicolas <nico.smael@gmail.com>
2022-07-30 21:05:00 -0300

    total tasks created counter

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux/es/exports";
 
 export const Header = () => {
 	const navigate = useNavigate();
 	const [user, setUser] = useState(localStorage.getItem("user"));
+
 	const handleLogOut = () => {
 		localStorage.removeItem("logged");
 		localStorage.removeItem("user");
 		navigate("/login", { replace: true });
 	};
+
+	const { tasks } = useSelector((state) => {
+		return state.taskReducer;
+	});
 
 	return (
 		<header>
@@ -16,6 +22,9 @@ export const Header = () => {
 			<div>
 				<div>{user}</div>
 				<div onClick={handleLogOut}>X</div>
+				<div>
+					Tareas creadas: {tasks?.length} <span></span>
+				</div>
 			</div>
 		</header>
 	);
