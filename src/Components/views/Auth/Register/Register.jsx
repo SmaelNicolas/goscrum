b94b2370960903cdc834c72afb609820f2a39c9f
Smael Nicolas <nico.smael@gmail.com>
2022-08-02 19:56:33 -0300

    add styles to register

--- src/Components/views/Auth/Register/Register.jsx
+++ src/Components/views/Auth/Register/Register.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { v4 as uuidv4 } from "uuid";
 import { Switch, FormControlLabel } from "@mui/material";
+import "./register.css";
 
 const { REACT_APP_API_ENDPOINT } = process.env;
 
@@ -93,141 +94,194 @@ export const Register = () => {
 	} = formik;
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<h1>Registro</h1>
-			<div>
-				<label>Nombre de usuario</label>
-				<input
-					name='userName'
-					type='text'
-					value={values.userName}
-					onChange={handleChange}
-					onBlur={handleBlur}
-				/>
-				{errors.userName && touched.userName && (
-					<div>{errors.userName}</div>
-				)}
-			</div>
-			<div>
-				<label>Email</label>
-				<input
-					name='email'
-					type='email'
-					value={values.email}
-					onChange={handleChange}
-					onBlur={handleBlur}
-				/>
-				{errors.email && touched.email && <div>{errors.email}</div>}
-			</div>
-			<div>
-				<label>Contraseña</label>
-				<input
-					type='password'
-					name='password'
-					value={values.password}
-					onChange={handleChange}
-					onBlur={handleBlur}
-				/>
-				{errors.password && touched.password && (
-					<div>{errors.password}</div>
-				)}
-			</div>
-			<div>
-				<label>Rol</label>
-				<select
-					name='role'
-					value={values.role}
-					onChange={handleChange}
-					onBlur={handleBlur}
-				>
-					<option value=''>Seleccionar rol ...</option>
-					{data?.Rol.map((rol) => (
-						<option value={rol} key={rol}>
-							{rol}
-						</option>
-					))}
-				</select>
-				{errors.role && touched.role && <div>{errors.role}</div>}
-			</div>
-			<div>
-				<label>Continente</label>
-				<select
-					name='continent'
-					value={values.continent}
-					onChange={(e) =>
-						handleChangeContinent(e.currentTarget.value)
-					}
-					onBlur={handleBlur}
-				>
-					<option value=''>Seleccionar continente ...</option>
-					{data?.continente.map((cont) => (
-						<option value={cont} key={cont}>
-							{cont}
-						</option>
-					))}
-				</select>
-				{errors.continent && touched.continent && (
-					<div>{errors.continent}</div>
-				)}
-			</div>
-			{values.continent === "America" && (
-				<div>
-					<label>Region</label>
-					<select
-						name='region'
-						value={values.region}
+		<div className='big--container'>
+			<form onSubmit={handleSubmit} className='form--container'>
+				<h1 className='h1--title'>Registro</h1>
+				<div className='register--form--section'>
+					<label className='form--label'>Nombre de usuario</label>
+					<input
+						name='userName'
+						type='text'
+						value={values.userName}
 						onChange={handleChange}
 						onBlur={handleBlur}
+						className={`form--input ${
+							errors.userName
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
+					/>
+				</div>
+				<div className='form--input--error'>
+					{errors.userName && touched.userName && (
+						<div>{errors.userName}</div>
+					)}
+				</div>
+				<div className='register--form--section'>
+					<label className='form--label'>Email</label>
+					<input
+						name='email'
+						type='email'
+						value={values.email}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						className={`form--input ${
+							errors.email
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
+					/>
+				</div>
+				<div className='form--input--error'>
+					{errors.email && touched.email && <div>{errors.email}</div>}
+				</div>
+				<div className='register--form--section'>
+					<label className='form--label'>Contraseña</label>
+					<input
+						type='password'
+						name='password'
+						value={values.password}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						className={`form--input ${
+							errors.password
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
+					/>
+				</div>
+				<div className='form--input--error'>
+					{errors.password && touched.password && (
+						<div>{errors.password}</div>
+					)}
+				</div>
+				<div className='register--form--section'>
+					<label className='form--label'>Rol</label>
+					<select
+						name='role'
+						value={values.role}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						className={`form--input ${
+							errors.role
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
 					>
-						<option value=''>Seleccionar region ...</option>
-						{data?.region.map((reg) => (
-							<option value={reg} key={reg}>
-								{reg}
+						<option value=''>Seleccionar rol ...</option>
+						{data?.Rol.map((rol) => (
+							<option value={rol} key={rol}>
+								{rol}
 							</option>
 						))}
 					</select>
+				</div>
+				<div className='form--input--error'>
+					{errors.role && touched.role && <div>{errors.role}</div>}
+				</div>
+				<div className='register--form--section'>
+					<label className='form--label'>Continente</label>
+					<select
+						name='continent'
+						value={values.continent}
+						onChange={(e) =>
+							handleChangeContinent(e.currentTarget.value)
+						}
+						onBlur={handleBlur}
+						className={`form--input ${
+							errors.continent
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
+					>
+						<option value=''>Seleccionar continente ...</option>
+						{data?.continente.map((cont) => (
+							<option value={cont} key={cont}>
+								{cont}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className='form--input--error'>
+					{errors.continent && touched.continent && (
+						<div>{errors.continent}</div>
+					)}
+				</div>
+				{values.continent === "America" && (
+					<div className='register--form--section'>
+						<label className='form--label'>Region</label>
+						<select
+							name='region'
+							value={values.region}
+							onChange={handleChange}
+							onBlur={handleBlur}
+							className={`form--input ${
+								errors.region
+									? "form--input--incorrect"
+									: "form--input--correct"
+							}`}
+						>
+							<option value=''>Seleccionar region ...</option>
+							{data?.region.map((reg) => (
+								<option value={reg} key={reg}>
+									{reg}
+								</option>
+							))}
+						</select>
+					</div>
+				)}
+				<div className='form--input--error'>
 					{errors.region && touched.region && (
 						<div>{errors.region}</div>
 					)}
 				</div>
-			)}
 
-			<FormControlLabel
-				control={
-					<Switch
-						value={values.switch}
-						onChange={() =>
-							formik.setFieldValue(
-								"switch",
-								!formik.values.switch
-							)
-						}
-						name='switch'
-						color='secondary'
-					/>
-				}
-				label='Pertenecés a un equipo ya creado'
-			/>
-			{values.switch && (
-				<div>
-					<label>
-						Por favor, introduce el identificador de equipo
-					</label>
-					<input
-						type='text'
-						name='teamID'
-						value={values.teamID}
-						onChange={handleChange}
-					/>
+				<FormControlLabel
+					control={
+						<Switch
+							value={values.switch}
+							onChange={() =>
+								formik.setFieldValue(
+									"switch",
+									!formik.values.switch
+								)
+							}
+							name='switch'
+							color='secondary'
+						/>
+					}
+					label='¿Ya pertenecés a un equipo?'
+				/>
+				{values.switch && (
+					<div className='register--form--section'>
+						<label className='form--label'>ID </label>
+						<input
+							type='text'
+							name='teamID'
+							value={values.teamID}
+							onChange={handleChange}
+						/>
+					</div>
+				)}
+				<div className='form--input--error'>
+					{errors.teamID && touched.teamID && (
+						<div>{errors.teamID}</div>
+					)}
 				</div>
-			)}
 
-			<div>
-				<button type='submit'>Enviar</button>
-			</div>
-			<div>
-				<Link to='/login'>Iniciar Sesion</Link>
-			</div>
-		</form>
+				<div>
+					<button type='submit' className='button--send'>
+						Crear Usuario
+					</button>
+				</div>
+				<div className='form--help--text'>¿Ya tenes una cuenta?</div>
+
+				<div>
+					<Link to='/login' className='link--othersection'>
+						Iniciar Sesion
+					</Link>
+				</div>
+			</form>
+		</div>
 	);
 };
