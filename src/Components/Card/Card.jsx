663bd4354bc675b0abe1077701faa3d03faa4d16
Smael Nicolas <nico.smael@gmail.com>
2022-08-03 13:06:26 -0300

    card styles

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./card.css";
 
 export const Card = ({
 	deleteTask,
@@ -18,28 +19,40 @@ export const Card = ({
 	const [showMore, setShowMore] = useState(false);
 
 	const reduceString = (str) => {
-		return str.length > 150
-			? { string: str.slice(0, 147).concat("..."), addButton: true }
+		return str.length > 110
+			? { string: str.slice(0, 107).concat("..."), addButton: true }
 			: { string: str, addButton: false };
 	};
 
 	return (
-		<div id='card'>
-			<div onClick={() => deleteTask(_id)}>X</div>
-			<h3>{title}</h3>
-			<h6>{dateTime}</h6>
-			<h5>{userName}</h5>
-			<button type='button' onClick={() => editTasksStatus(data)}>
-				{" "}
+		<div id='card' className={`card card--${importance}--${status}`}>
+			<div className='card--delete' onClick={() => deleteTask(_id)}>
+				X
+			</div>
+			<h3 className='card--title'>{title}</h3>
+			<div className='card--time'>{dateTime}</div>
+			<div className='card--user'>{userName}</div>
+			<button
+				className={`card--button ${status}`}
+				type='button'
+				onClick={() => editTasksStatus(data)}
+			>
 				{status}
 			</button>
-			<button type='button'> {importance}</button>
-			<p style={{ width: "500px" }}>
+			<button className={`card--button ${importance}`} type='button'>
+				{importance}
+			</button>
+			<p className='card--description'>
 				{!showMore ? reduceString(description).string : description}
 			</p>
-			<button onClick={() => setShowMore(!showMore)}>
-				{!showMore ? "Ver Mas" : "Ver Menos"}
-			</button>
+			{reduceString(description).addButton && (
+				<button
+					className='card--button'
+					onClick={() => setShowMore(!showMore)}
+				>
+					{!showMore ? "Ver Mas" : "Ver Menos"}
+				</button>
+			)}
 		</div>
 	);
 };
