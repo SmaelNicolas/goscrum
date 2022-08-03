a2d6647aca9fafbb925f6e92342c71b037b2edfd
Smael Nicolas <nico.smael@gmail.com>
2022-08-03 13:06:12 -0300

    task form styles

--- src/Components/TaskForm/TaskForm.jsx
+++ src/Components/TaskForm/TaskForm.jsx
@@ -2,6 +2,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./taskForm.css";
 
 const { REACT_APP_API_ENDPOINT: API_ENDPOINT } = process.env;
 
@@ -16,9 +17,7 @@ export const TaskForm = () => {
 	const requiered = "* Campo Obligatorio";
 
 	const validationSchema = Yup.object().shape({
-		title: Yup.string()
-			.min(6, "La cantidad minima es 6")
-			.required(requiered),
+		title: Yup.string().min(4, "Minimo 4 caracteres").required(requiered),
 		status: Yup.string().required(requiered),
 		importance: Yup.string().required(requiered),
 		description: Yup.string().required(requiered),
@@ -55,78 +54,104 @@ export const TaskForm = () => {
 	} = formik;
 
 	return (
-		<div>
-			<h2>Crear Tarea</h2>
-			<p>Crea tus tareas</p>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<div>
-						<input
-							value={values.title}
-							name='title'
-							placeholder='Titulo'
-							onChange={handleChange}
-							onBlur={handleBlur}
-							className={errors.title ? "error" : "sinError"}
-						/>
-						{errors.title && touched.title && (
-							<span>{errors.title}</span>
-						)}
-					</div>
-					<div>
-						<select
-							value={values.status}
-							name='status'
-							id=''
-							onChange={handleChange}
-							onBlur={handleBlur}
-							className={errors.title ? "error" : "sinError"}
-						>
-							<option value=''>Seleccionar Opcion</option>
-							<option value='NEW'>Nueva</option>
-							<option value='IN PROGRESS'>En Proceso</option>
-							<option value='FINISHED'>Terminada</option>
-						</select>
-						{errors.status && touched.status && (
-							<span>{errors.status}</span>
-						)}
-					</div>
-					<div>
-						<select
-							value={values.importance}
-							name='importance'
-							id=''
-							onChange={handleChange}
-							onBlur={handleBlur}
-							className={errors.title ? "error" : "sinError"}
-						>
-							<option value=''>Seleccionar Opcion</option>
-							<option value='LOW'>Baja</option>
-							<option value='MEDIUM'>Media</option>
-							<option value='HIGH'>Alta</option>
-						</select>
-						{errors.importance && touched.importance && (
-							<span>{errors.importance}</span>
-						)}
-					</div>
+		<div className='big--container'>
+			<h2 className='h2--title'>Crear Tarea</h2>
+			<form onSubmit={handleSubmit} className='form--container'>
+				<div className='task--form--section'>
+					<label className='form--label'>Titulo</label>
+
+					<input
+						value={values.title}
+						name='title'
+						onChange={handleChange}
+						onBlur={handleBlur}
+						className={`form--input--task ${
+							errors.title
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
+					/>
 				</div>
-				<div>
+				<div className='form--input--error'>
+					{errors.title && touched.title && (
+						<span>{errors.title}</span>
+					)}
+				</div>
+				<div className='task--form--section'>
+					<label className='form--label'>Status</label>
+					<select
+						value={values.status}
+						name='status'
+						id=''
+						onChange={handleChange}
+						onBlur={handleBlur}
+						className={`form--input--task ${
+							errors.status
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
+					>
+						<option value=''>Seleccionar Opcion</option>
+						<option value='NEW'>Nueva</option>
+						<option value='IN PROGRESS'>En Proceso</option>
+						<option value='FINISHED'>Terminada</option>
+					</select>
+				</div>
+				<div className='form--input--error'>
+					{errors.status && touched.status && (
+						<span>{errors.status}</span>
+					)}
+				</div>
+				<div className='task--form--section'>
+					<label className='form--label'>Prioridad</label>
+					<select
+						value={values.importance}
+						name='importance'
+						id=''
+						onChange={handleChange}
+						onBlur={handleBlur}
+						className={`form--input--task ${
+							errors.importance
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
+					>
+						<option value=''>Seleccionar Opcion</option>
+						<option value='LOW'>Baja</option>
+						<option value='MEDIUM'>Media</option>
+						<option value='HIGH'>Alta</option>
+					</select>
+				</div>
+				<div className='form--input--error'>
+					{errors.importance && touched.importance && (
+						<span>{errors.importance}</span>
+					)}
+				</div>
+				<div className='task--form--section'>
+					<label className='form--label'>Descripción</label>
 					<textarea
 						name='description'
 						id=''
 						cols='30'
 						rows='10'
-						placeholder='Descripcion'
 						value={values.description}
 						onChange={handleChange}
 						onBlur={handleBlur}
-						className={errors.title ? "error" : "sinError"}
+						className={`form--input--task ${
+							errors.description
+								? "form--input--incorrect"
+								: "form--input--correct"
+						}`}
 					/>
+				</div>
+				<div className='form--input--error'>
 					{errors.description && touched.description && (
 						<span>{errors.description}</span>
 					)}
 				</div>
-				<button type='submit'>Crear</button>
+				<button type='submit' className='button--send'>
+					Crear
+				</button>
 			</form>
 			<ToastContainer />
 		</div>
