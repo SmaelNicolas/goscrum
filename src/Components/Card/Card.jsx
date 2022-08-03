8516ff83fc3bf8f80918d661307e8c9f1791ada0
Smael Nicolas <nico.smael@gmail.com>
2022-08-03 13:24:08 -0300

    card styles

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -26,12 +26,20 @@ export const Card = ({
 
 	return (
 		<div id='card' className={`card card--${importance}--${status}`}>
-			<div className='card--delete' onClick={() => deleteTask(_id)}>
-				X
-			</div>
-			<h3 className='card--title'>{title}</h3>
+			{status === "FINISHED" && (
+				<button
+					className='card--delete'
+					onClick={() => deleteTask(_id)}
+				>
+					X
+				</button>
+			)}
+			<div className='card--title'>{title}</div>
+			<label className='card--label'>Fecha</label>
 			<div className='card--time'>{dateTime}</div>
+			<label className='card--label'>Usuario</label>
 			<div className='card--user'>{userName}</div>
+			<label className='card--label'>Estado </label>
 			<button
 				className={`card--button ${status}`}
 				type='button'
@@ -39,9 +47,13 @@ export const Card = ({
 			>
 				{status}
 			</button>
+			<label className='card--label'>Importancia </label>
+
 			<button className={`card--button ${importance}`} type='button'>
 				{importance}
 			</button>
+			<label className='card--label'>Mensaje </label>
+
 			<p className='card--description'>
 				{!showMore ? reduceString(description).string : description}
 			</p>
