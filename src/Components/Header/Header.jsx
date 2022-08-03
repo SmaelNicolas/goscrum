a6eeec9a43a7c7e97be0c1cc26967c957737478c
Smael Nicolas <nico.smael@gmail.com>
2022-08-03 13:39:32 -0300

    header  style import

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux/es/exports";
 import "./header.css";
+import { useResize } from "../../Hooks/useResize";
 
 export const Header = () => {
 	const navigate = useNavigate();
 	const [user, setUser] = useState(localStorage.getItem("user"));
+
+	const { isMobile } = useResize();
 
 	const handleLogOut = () => {
 		localStorage.removeItem("logged");
@@ -17,7 +20,7 @@ export const Header = () => {
 		return state.taskReducer;
 	});
 
-	return (
+	return isMobile ? (
 		<header className='header'>
 			<span>Go Scrum</span>
 			<button onClick={() => navigate("/donate", { replace: true })}>
@@ -25,7 +28,22 @@ export const Header = () => {
 			</button>
 			<div>
 				<div>{user}</div>
-				<div onClick={handleLogOut}>X</div>
+				<button onClick={handleLogOut}>Cerrar Sesion</button>
+
+				<div>
+					Tareas creadas: {tasks?.length} <span></span>
+				</div>
+			</div>
+		</header>
+	) : (
+		<header className='header'>
+			<span>Go Scrum</span>
+			<button onClick={() => navigate("/donate", { replace: true })}>
+				Donar
+			</button>
+			<div>
+				<div>{user}</div>
+				<button onClick={handleLogOut}>Cerrar Sesion</button>
 				<div>
 					Tareas creadas: {tasks?.length} <span></span>
 				</div>
