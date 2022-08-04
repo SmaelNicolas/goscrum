7d6fa0ef0dcd176f2e9633ba98b0fe10f919e881
Smael Nicolas <nico.smael@gmail.com>
2022-08-04 14:40:08 -0300

    errors screen TODO

---
+++ src/Components/ScreenErrors/ApiError.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ApiError = () => {
+	return (
+		<div>
+			<span>Hubo un error ajeno a vos.</span>
+			<span>Prueba mas tarde.</span>
+		</div>
+	);
+};
---
+++ src/Components/ScreenErrors/NoTasksError.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NoTasksError = () => {
+	return <div>No hay tareas creadas</div>;
+};
