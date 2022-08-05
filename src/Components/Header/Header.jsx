88cee88e2e521daf6ddeee744abe69987503460f
Smael Nicolas <nico.smael@gmail.com>
2022-08-05 13:28:45 -0300

    logout clenaning

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux/es/exports";
 import { useResize } from "../../Hooks/useResize";
 import { MobileMenu } from "./MobileMenu/MobileMenu";
 import { DesktopMenu } from "./DesktopMenu/DesktopMenu";
+import { useSelector, useDispatch } from "react-redux";
+
 import "./header.css";
+import { tasksLogout } from "../../store/actions/tasksActions";
 
 export const Header = () => {
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const [user] = useState(localStorage.getItem("user"));
 
 	const { isMobile } = useResize();
 
 	const handleLogOut = () => {
-		localStorage.removeItem("logged");
+		localStorage.removeItem("token");
 		localStorage.removeItem("user");
+		dispatch(tasksLogout());
 		navigate("/login", { replace: true });
 	};
 
