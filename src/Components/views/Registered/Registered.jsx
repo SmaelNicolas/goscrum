528a93c7c07e799c872daca2fd815131d59b3ecb
Smael Nicolas <nico.smael@gmail.com>
2022-08-08 15:28:07 -0300

    dlt

--- src/Components/views/Registered/Registered.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-export const Registered = () => {
-	const { teamID } = useParams();
-
-	return <div>TEAM ID : {teamID} </div>;
-};
