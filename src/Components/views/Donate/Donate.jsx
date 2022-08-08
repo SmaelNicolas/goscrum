39390124aa4ec6f755d05dfb17d798eb8b8c3771
Smael Nicolas <nico.smael@gmail.com>
2022-08-08 15:27:45 -0300

    donate page styling

--- src/Components/views/Donate/Donate.jsx
+++ src/Components/views/Donate/Donate.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./donate.css";
+import imgDonate from "../../../assets/donate.jpg";
 
 export const Donate = () => {
+	const navigate = useNavigate();
 	return (
-		<div>
-			<h3>Colabora con nosotros</h3>
-			<a href='https://mpago.la/1haC6Zw' target='_blank' rel='noreferrer'>
+		<div className='donate--container'>
+			<h2 className='h2--title'>Colabora con nosotros</h2>
+			<a
+				className='donate--link'
+				href='https://mpago.la/1haC6Zw'
+				target='_blank'
+				rel='noreferrer'
+			>
 				Donar
 			</a>
+			<img className='donate--img' src={imgDonate} alt='donate' />
+			<button
+				className='header--button--donate'
+				onClick={() => navigate("/", { replace: true })}
+			>
+				Volver a Tareas
+			</button>
 		</div>
 	);
 };
